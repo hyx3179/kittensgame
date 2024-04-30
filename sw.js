d6822d82da105ac9bf14fe228e24dca24ecfcb4c
hyx3179 <hyx3179@gmail.com>
2024-05-01 02:37:45 +0800

    serviceWorker 设置选项 更新机制还原

--- sw.js
+++ sw.js
@@ -1,36 +1,7 @@
 (function () {
+  const swRevision = 0;
+  // 这之前的行会在构建时删除 --------------------------
   const HOSTNAME_BLACKLIST = ["kittensgame.com"];
-
-  const getFetch = (cached, url) => {
-    if (cached && cached.ok) {
-      return cached;
-    }
-    // 不存在 cache 或响应不在成功范围，则请求网络
-    // 删除以前可能存在的版本并提交 cache
-    // 跨域资源单独存放
-    const req = new Request(url.href, {
-      cache: "no-store",
-      mode: url.host !== location.host ? "cors" : "no-cors",
-    });
-    return Promise.all([
-      fetch(req),
-      caches.open(url.host !== location.host ? "catExtRes" : "catRes"),
-    ])
-      .then(([response, cache]) => {
-        if (!response.ok) {
-          return Promise.reject();
-        }
-        if (url.host.indexOf("fonts.g") === -1) {
-          cache.delete(req, { ignoreSearch: true });
-        }
-        return cache.put(req, response);
-      })
-      .then(() => caches.match(req))
-      .catch(() =>
-        // 无网络返回已有缓存
-        caches.match(req, { ignoreSearch: true })
-      );
-  };
 
   // 请求处理
   addEventListener("fetch", (event) => {
@@ -40,25 +11,39 @@
       return;
     }
 
+    const cacheName = url.host !== location.host ? "catExtRes" : "catRes";
+    // index 添加查询参数
+    if (url.pathname === "/") {
+      url.search = "?rev_=" + swRevision;
+    }
+    // 跨域资源单独存放
+    const req = new Request(url.href, {
+      cache: "no-store",
+      mode: url.host !== location.host ? "cors" : "no-cors",
+    });
     // 优先寻找 cache
     event.respondWith(
-      caches.match(event.request).then((cached) => {
-        if (url.pathname === "/") {
-          return fetch("build.version.json?=" + Date.now())
-            .then((response) => response.json())
-            .then((json) => {
-              url.search = "?rev_=" + json.swRevision;
-              const reqroot = new Request(url);
-              return caches.match(reqroot);
-            })
-            .then((cached) => getFetch(cached, url))
-            .catch(() =>
-              // 无网络返回已有缓存
-              caches.match(event.request, { ignoreSearch: true })
-            );
-        } else {
-          return getFetch(cached, url);
+      caches.match(req).then((cached) => {
+        if (cached && cached.ok) {
+          return cached;
         }
+        return (
+          // 不存在 cache 或响应不在成功范围，则请求网络
+          Promise.all([fetch(req), caches.open(cacheName)])
+            .then(([response, cache]) => {
+              if (!response.ok) {
+                return Promise.reject();
+              }
+              if (url.host.indexOf("fonts.g") === -1) {
+                // 删除以前可能存在的版本并提交 cache
+                cache.delete(req, { ignoreSearch: true });
+              }
+              return cache.put(req, response);
+            })
+            .then(() => caches.match(req))
+            // 无网络返回已有缓存
+            .catch(() => caches.match(req, { ignoreSearch: true }))
+        );
       })
     );
   });
