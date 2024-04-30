11c1d51503d05174b4c48b8e43a91d1412c4b6cb
hyx3179 <hyx3179@gmail.com>
2024-05-01 02:14:56 +0800

    serviceWorker 升级

--- sw.js
+++ sw.js
@@ -1,92 +1,65 @@
 (function () {
-	const swRevision = 0
-	// 这之前的行会在构建时删除 --------------------------
-	const cacheStorage = 'cat-' + swRevision
-	const HOSTNAME_BLACKLIST = [
-		'hm.baidu.com',
-		'p.qlogo.cn',
-		'q2.qlogo.cn',
-		'kittensgame.com',
-		'www.google-analytics.com',
-	]
-	const host = location.host
+  const HOSTNAME_BLACKLIST = ["kittensgame.com"];
 
-	// 获取请求
-	const getRequest = (url) => {
-		url = new URL(url)
-		let reqList = new Array()
+  const getFetch = (cached, url) => {
+    if (cached && cached.ok) {
+      return cached;
+    }
+    // 不存在 cache 或响应不在成功范围，则请求网络
+    // 删除以前可能存在的版本并提交 cache
+    // 跨域资源单独存放
+    const req = new Request(url.href, {
+      cache: "no-store",
+      mode: url.host !== location.host ? "cors" : "no-cors",
+    });
+    return Promise.all([
+      fetch(req),
+      caches.open(url.host !== location.host ? "catExtRes" : "catRes"),
+    ])
+      .then(([response, cache]) => {
+        if (!response.ok) {
+          return Promise.reject();
+        }
+        if (url.host.indexOf("fonts.g") === -1) {
+          cache.delete(req, { ignoreSearch: true });
+        }
+        return cache.put(req, response);
+      })
+      .then(() => caches.match(req))
+      .catch(() =>
+        // 无网络返回已有缓存
+        caches.match(req, { ignoreSearch: true })
+      );
+  };
 
-		let cors = url.host !== host ? 'cors' : 'no-cors'
-		let newReq = new Request(url.href, { mode: cors })
-		reqList.push(newReq)
+  // 请求处理
+  addEventListener("fetch", (event) => {
+    let url = new URL(event.request.url);
+    // 跳过无法正确处理 cors 的请求和不应缓存的请求
+    if (HOSTNAME_BLACKLIST.indexOf(url.host) !== -1) {
+      return;
+    }
 
-		return reqList
-	}
-	// 根据请求列表返回 fetchList
-	const getFetch = (req) => {
-		let fetchList = []
-		for (let i in req) {
-			fetchList.push(fetch(req[i], { cache: 'no-store' })
-				.then(resp => {
-					if (!resp.ok) {
-						return new Promise(function (resolve) {
-							setTimeout(resolve, 5000, resp);
-						});
-					}
-					return resp
-				}))
-		}
-		return fetchList
-	}
-
-	// 安装：加载 index 到 cache
-	addEventListener('install', event => {
-		event.waitUntil(caches.open(cacheStorage).then(cache => cache.add('/')));
-		//skipWaiting()
-	});
-
-	// 激活：删除之前的 cache
-	addEventListener('activate', event => {
-		event.waitUntil(Promise.all([
-			clients.claim(),
-			caches.keys().then(cacheNames =>
-				Promise.all(cacheNames.filter(cacheName => {
-					cacheName = cacheName.split('-')
-					return cacheName.length == 2 && cacheName[0] === 'cat' && cacheName[1] !== swRevision.toString()
-				}).map(cacheName => caches.delete(cacheName))))]))
-	})
-
-	// 请求处理
-	addEventListener('fetch', event => {
-		const url = new URL(event.request.url)
-		// 跳过无法正确处理 cors 的请求和不应缓存的请求
-		if (HOSTNAME_BLACKLIST.indexOf(url.host) > -1) { return }
-
-		// 跨域资源单独存放
-		let cacheName = url.host !== host ? 'catStaticRes' : cacheStorage
-
-		const req = getRequest(url.href)
-		// 优先寻找 cache
-		event.respondWith(caches.match(req[0])
-			.then(cached => {
-				if (cached && cached.ok) { return cached }
-				// 不存在 cache 或响应不在成功范围，则请求网络
-				// 删除以前可能存在的版本并提交 cache
-				// race() 返回最快的成功响应
-				return Promise.race(getFetch(req))
-					.then(resp => {
-						const respCopy = resp.clone()
-						Promise.all([respCopy, caches.open(cacheName)])
-							.then(([response, cache]) => {
-								req[0].url.indexOf('fonts.g') == -1 && cache.delete(req[0], { ignoreSearch: true })
-								cache.put(req[0], response)
-							})
-						return resp
-					})
-					.catch(a => { // 无网络返回已有缓存
-						return caches.match(req[0], { ignoreSearch: true })
-							.then(resp => { return resp })
-					})
-			}))
-	})
-})()
+    // 优先寻找 cache
+    event.respondWith(
+      caches.match(event.request).then((cached) => {
+        if (url.pathname === "/") {
+          return fetch("build.version.json?=" + Date.now())
+            .then((response) => response.json())
+            .then((json) => {
+              url.search = "?rev_=" + json.swRevision;
+              const reqroot = new Request(url);
+              return caches.match(reqroot);
+            })
+            .then((cached) => getFetch(cached, url))
+            .catch(() =>
+              // 无网络返回已有缓存
+              caches.match(event.request, { ignoreSearch: true })
+            );
+        } else {
+          return getFetch(cached, url);
+        }
+      })
+    );
+  });
+})();
